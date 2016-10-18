8c164cce414e25ecf1f1da26399cbdaf86d39561
Shripal Soni <shripalsoni04@gmail.com>
2016-10-18 20:14:43 +0530

    Styling for android statusbar. Not showing activity-indicator for Favourite quotes page.

--- nativescript/app/app.component.ts
+++ nativescript/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
+import { SwissArmyKnife } from 'nativescript-swiss-army-knife';
 
 @Component({
   selector: 'my-app',
   template: '<page-router-outlet></page-router-outlet>'
 })
 export class AppComponent {
+  constructor() {
+    SwissArmyKnife.setAndroidStatusBarColor('#01579B');
+  }
 }
