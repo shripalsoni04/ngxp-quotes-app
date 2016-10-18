74e144cefe5c8bee6d39598619342da4cb7ebc63
Shripal Soni <shripalsoni04@gmail.com>
2016-10-19 01:32:03 +0530

    Animating quote-card to off the screen when quotes are un-favourited from favourite quotes page.

--- nativescript/app/quotes/quotes-list/quotes-list.component.ts
+++ nativescript/app/quotes/quotes-list/quotes-list.component.ts
@@ -1,10 +1,12 @@
 import {
-  Component, OnChanges, Input, ChangeDetectionStrategy
+  Component, OnChanges, Input, ChangeDetectionStrategy, ChangeDetectorRef
 } from '@angular/core';
 
 import { QuotesListVM } from './quotes-list.view-model';
 
 import { TNSFontIconService } from 'nativescript-ng2-fonticon';
+
+import { StackLayout } from 'ui/layouts/stack-layout';
 
 declare const UITableViewCellSelectionStyle: any;
 
@@ -21,7 +23,11 @@ export class QuotesListComponent implements OnChanges {
 
   @Input() quotesBy: 'author' | 'category' | 'all' | 'favourites';
 
-  constructor(public vm: QuotesListVM, private fonticon: TNSFontIconService) {
+  constructor(
+    public vm: QuotesListVM,
+    private fonticon: TNSFontIconService,
+    private changeDetectorRef: ChangeDetectorRef
+  ) {
 
   }
 
@@ -34,6 +40,30 @@ export class QuotesListComponent implements OnChanges {
     if (args.ios) {
       let cell = args.ios;
       cell.selectionStyle = UITableViewCellSelectionStyle.UITableViewCellSelectionStyleNone;
+    }
+  }
+
+  toggleFav(quoteCardContainer: StackLayout, quote: any, quotesBy) {
+    if (quotesBy === 'favourites') {
+      // if quotesList is for favourites quotes, then on toggle of favourite
+      // button, removing quoteCard by animating it to off the screen.
+      quoteCardContainer.animate({
+        translate: { x: 1000, y: 0 },
+        duration: 300,
+        opacity: 0
+      }).then(() => {
+        this.vm.toggleFav(quote, quotesBy).then(() => {
+          // as angular is not removing the actual stackLayout if the number of
+          // items are more than that can be accomodated in screen and it is
+          // only changing the data, we need to revert the stackLayout opacity
+          // and tranlateX properties.
+          quoteCardContainer.opacity = 1;
+          quoteCardContainer.translateX = 0;
+          this.changeDetectorRef.markForCheck();
+        });
+      });
+    } else {
+      this.vm.toggleFav(quote, quotesBy);
     }
   }
 }
